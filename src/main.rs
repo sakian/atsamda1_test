056a7c21ff1ec64f439ac2a43e1f3c1b27cabb6e
Mark <mark@opifex.ca>
2022-08-30 17:06:13 -0400

    Added repeated message

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,32 @@
 
 use panic_halt as _;
 
-use atsamd_hal as _;
+use atsamd_hal as hal;
 use cortex_m_rt::entry;
+use hal::clock::GenericClockController;
+use hal::delay::Delay;
+use hal::pac::{CorePeripherals, Peripherals};
+use hal::prelude::*;
 use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
-    rprintln!("Started");
-    loop {}
+
+    let core = CorePeripherals::take().unwrap();
+    let mut peripherals = Peripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_internal_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.PM,
+        &mut peripherals.SYSCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let mut delay = Delay::new(core.SYST, &mut clocks);
+
+    let mut count = 0;
+    loop {
+        count += 1;
+        rprintln!("Count: {}", count);
+        delay.delay_ms(100u8);
+    }
 }
