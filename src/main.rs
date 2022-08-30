57a01bdc2b7bb151c79039bd11b4c33a38f2e42e
Mark <mark@opifex.ca>
2022-08-30 10:10:29 -0400

    Remove defmt and depend only on RTT stuff

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 #![no_std]
 #![no_main]
 
-use defmt_rtt as _;
 use panic_probe as _;
 
 use atsamd_hal as _;
 use cortex_m_rt::entry;
+use rtt_target::{rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
-    defmt::info!("Started");
+    rtt_init_print!();
+    rprintln!("Started");
     loop {}
 }
